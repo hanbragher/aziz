6740c6350b2576603b975555558f05396a4a7587
Pavel <hanbragher@gmail.com>
2018-10-04 17:31:11 +0400

    localization

--- config/app.php
+++ config/app.php
@@ -151,6 +151,9 @@ return [
          * Package Service Providers...
          */
 
+        Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
+
+
         /*
          * Application Service Providers...
          */
@@ -208,6 +211,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'LaravelLocalization' => Mcamara\LaravelLocalization\Facades\LaravelLocalization::class,
 
     ],
 
